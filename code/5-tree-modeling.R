b6c89837afc17a693b87c0a9cc7ce46656365062
Kennedy Manley <kennedymanley@kennedys-mbp-2.myfiosgateway.com>
2021-12-17 12:09:42 -0500

    worked on project

--- code/5-tree-modeling.R
+++ code/5-tree-modeling.R
@@ -10,43 +10,37 @@ library(cowplot)       # for side by side plots
 
 # read in the training data
 grad_train = read_csv("data/clean/grad_train.csv")
-grad_train = grad_train %>% na.omit(grad_train)
-grad_train_clean = grad_train %>% select(-report_school_year, -aggregation_index, -aggregation_name,
-                                         -county_code, -county_name, -membership_code, -membership_key,
-                                         -membership_desc, -subgroup_code, -subgroup_name)
+
+set.seed(1)
+randomRows = sample(1:nrow(grad_train), 1000, replace=F)
+sliced_data = grad_train %>% slice(randomRows) %>% na.omit(grad_train) %>%
+  select(-report_school_year, -aggregation_index, -aggregation_name,
+         -county_code, -county_name, -membership_code, -membership_key,
+         -membership_desc, -subgroup_code, -subgroup_name)
 
 # fit classification tree
-tree_fit = rpart(grad_rate ~ . ,
+tree_fit1 = rpart(grad_rate ~ . ,
                  method = "class", # classification
                  parms = list(split = "gini"), # Gini index for splitting
-                 data = grad_train_clean)
+                 data = sliced_data)
 
-rpart.plot(tree_fit) # plot tree
+
+save(tree_fit1, file = "results/tree_fit.Rda")
+rpart.plot(tree_fit1, box.palette=0) # plot tree
 
 #fit deepest possible tree
 set.seed(1) # for reproducibility (DO NOT CHANGE)
-
-deepest_tree_fit = rpart(spam ~ .,
+deepest_tree_fit = rpart(grad_rate ~ .,
                          method = "class",
                          parms = list(split = "gini"),
                          control = rpart.control(minsplit = 1,
                                                  minbucket = 1,
                                                  cp = 0),
-                         data = spam_train)
+                         data = sliced_data)
 
 # print the CP table for this tree
 cp_table = deepest_tree_fit$cptable %>% as_tibble()
-cp_table %>% kable(format = "latex", row.names = NA, 
-                   booktabs = TRUE, digits = 5,
-                   col.names = c("CP", 
-                                 "Number of splits",
-                                 "Relative error",
-                                 "CV error",
-                                 "CV std"),
-                   caption = "This is the CP table for this 
-                   deepest tree.") %>%
-  kable_styling(position = "center") %>%
-  kable_styling(latex_options = "HOLD_position")
+save(cp_table, file = "results/cp_table.Rda")
 
 # produce CV plot based on info in CP table
 cp_table %>%
@@ -66,42 +60,24 @@ optimal_tree_info = cp_table %>%
 optimal_tree_info$nsplit
 
 
-optimal_tree = prune(tree = tree_fit, cp = optimal_tree_info$CP)
+optimal_tree = prune(tree = tree_fit1, cp = optimal_tree_info$CP)
+save(optimal_tree, file = "results/optimal_tree.Rda")
 
 
 #random forest
 set.seed(1) # for reproducibility (DO NOT CHANGE)
+rf_fit = randomForest(factor(grad_rate) ~ ., data = sliced_data)
+save(rf_fit, file = "results/rf_fit.Rda")
 
-rf_fit = randomForest(factor(spam) ~ ., data = spam_train)
+num_features = ncol(sliced_data) - 1
+mtry = floor(sqrt(num_features))
+mtry_verified = rf_fit$mtry
 
-
-#boosted models
-set.seed(1) # for reproducibility (DO NOT CHANGE)
-# fit random forest with interaction depth 1
-gbm_fit_1 = gbm(spam ~ .,
-                distribution = "bernoulli", 
-                n.trees = 1000, 
-                interaction.depth = 1, 
-                shrinkage = 0.1,
-                cv.folds = 5,
-                data = spam_train)
-
-set.seed(1) # for reproducibility (DO NOT CHANGE)
-# fit random forest with interaction depth 2
-gbm_fit_2 = gbm(spam ~ .,
-                distribution = "bernoulli", 
-                n.trees = 1000, 
-                interaction.depth = 2, 
-                shrinkage = 0.1,
-                cv.folds = 5,
-                data = spam_train)
-
-set.seed(1) # for reproducibility (DO NOT CHANGE)
-# fit random forest with interaction depth 3
-gbm_fit_3 = gbm(spam ~ .,
-                distribution = "bernoulli", 
-                n.trees = 1000, 
-                interaction.depth = 3, 
-                shrinkage = 0.1,
-                cv.folds = 5,
-                data = spam_train)
+o = tibble(oob_error = rf_fit$err.rate[,"OOB"], trees = 1:500) %>%
+  ggplot(aes(x = trees, y = oob_error)) + geom_line() + 
+  labs(x = "Number of trees", y = "OOB error") + theme_bw()
+ggsave(filename = "results/oob-error-plot.png", 
+       plot = o, 
+       device = "png", 
+       width = 6, 
+       height = 4)
--- code/6-model-evaluation.R
+++ code/6-model-evaluation.R
@@ -3,7 +3,7 @@ library(glmnetUtils)
 library(tidyverse)
 
 # load test data
-covid_test = read_tsv("data/clean/covid_test.tsv")
+grad_test = read_csv("data/clean/grad_test.csv")
 
 # load ridge fit object
 load("results/ridge_fit.Rda")
@@ -13,18 +13,18 @@ load("results/lasso_fit.Rda")
 
 # evaluate ridge RMSE
 ridge_predictions = predict(ridge_fit, 
-                            newdata = covid_test, 
+                            newdata = grad_test, 
                             s = "lambda.1se") %>%
   as.numeric()
-ridge_RMSE = sqrt(mean((ridge_predictions-covid_test$case_fatality_rate)^2))
+ridge_RMSE = sqrt(mean((ridge_predictions-grad_test$grad_rate)^2))
 
 # evaluate lasso RMSE
 lasso_predictions = predict(lasso_fit, 
-                            newdata = covid_test, 
+                            newdata = grad_test, 
                             s = "lambda.1se") %>%
   as.numeric()
-lasso_RMSE = sqrt(mean((lasso_predictions-covid_test$case_fatality_rate)^2))
+lasso_RMSE = sqrt(mean((lasso_predictions-grad_test$grad_rate)^2))
 
 # print nice table
 tibble(Method = c("Ridge", "Lasso"), `Test RMSE` = c(ridge_RMSE, lasso_RMSE)) %>%
-  write_tsv("results/model-evaluation.tsv")
+  write_csv("results/model-evaluation.csv")
